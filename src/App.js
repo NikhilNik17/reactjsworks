34a21f2937b05dc8cb6492cc38651890531d2859
MANDELA NARASIMHA NIKHIL <165284427+NikhilNik17@users.noreply.github.com>
2024-09-17 12:32:04 +0530

    17.08.latest

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
     <div className='container-fluid'>
     <Router>
       <Routes>
-        <Route path='/login' element={ <Login />} />
+        
+        <Route path='/reactjsworks' element={ <Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/phones' element={<Phones />} />
