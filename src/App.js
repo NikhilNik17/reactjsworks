06df20b825105dd49568f49c55fd8a672ac1c155
MANDELA NARASIMHA NIKHIL <165284427+NikhilNik17@users.noreply.github.com>
2024-09-18 11:24:43 +0530

    222

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Router>
       <Routes>
         
-        <Route path='/' element={ <Login />} />
+        <Route path='/login' element={ <Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/phones' element={<Phones />} />
